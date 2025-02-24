2dd317fea903186c200237be9c827d1a9d24b1c4
slivapyt <faer2325@gmail.com>
2025-02-25 04:59:46 +0700

    создал первую модель и сериализатор

--- users/models.py
+++ users/models.py
@@ -2,5 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+optional_field = {'blank': True, 'null': True}
+
+
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, **optional_field)
+    first_name = models.CharField(max_length=30, **optional_field)
+    last_name = models.CharField(max_length=30, **optional_field)
+    profile_picture = models.ImageField(
+        upload_to='profile_picture/', **optional_field)
+
+    def __str__(self):
+        return self.username
